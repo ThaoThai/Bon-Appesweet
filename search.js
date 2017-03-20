3560e9dfacc2cc24a53b4ca81176bf912d431e9a
geovannilei <geovanni3557@gmail.com>
2017-03-20 13:27:12 -0500

    Add files via upload

--- search.js
+++ search.js
@@ -1,53 +1,99 @@
 /**
- * Created by geova on 2/23/2017.
+ * http://usejsdoc.org/
  */
-function search(item){
-    //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xhttp = new XMLHttpRequest();
-    var databox = [];
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var Jsoninfo = JSON.parse(this.responseText);
-            for(var i=1; i< Jsoninfo.count;i++){
-                var object = {};
-                var ingredients = [];
-                try{
-                    for(var j = 0; j<Jsoninfo.hits[i].recipe.ingredients.length;j++){
-                        ingredients.push(Jsoninfo.hits[i].recipe.ingredients[j]['text']);
-                    }
-                    object = {label: Jsoninfo.hits[i].recipe.label, image:Jsoninfo.hits[i].recipe.image,ingredients:ingredients};
-                    databox.push(object);
-                }catch(undefine){
-                    i++;
-                }
-            }
-            insertable(databox);
-                }
-    };
-    xhttp.open("GET", "https://api.edamam.com/search?q=" + item +"&app_id=c31678fe&app_key=9f0a83112ad1a27f64190bf197af11c2&from=0&to=1000"
-        , true);
-    xhttp.send();
-}
-function insertable(databox) {
-    console.log(databox);
-    var body = document.getElementById('body');
-    for (var k = 0; k < databox.length; k++) {
-        var tr = document.createElement('tr');
-        body.appendChild(tr);
-        var td1 = document.createElement('td');
-        var td2 = document.createElement('td');
-        var td3 = document.createElement('td');
-            //tr.innerHTML = "<td><img src='" + databox[k].image + "'/></td><td>" + databox[k].label + "</td><td id='ingredients" + k + "'></td>";
-        td1.innerHTML = "<img src='" + databox[k].image + "'/>";
-        td2.innerHTML = databox[k].label;
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-            for (var p = 0; p < databox[k].ingredients.length; p++) {
-                var tr2=document.createElement('tr');
-                tr2.innerHTML ='<tr>' + databox[k].ingredients[p] + '</tr>';
-                td3.appendChild(tr2);
-            }
-        }
+function search(fillIngredients,limitLicense,ranking){
+    var Ingredients = document.getElementById("search").value;
+    Ingredients = Ingredients.replace(',','%');
+    var number = document.getElementById("number").value;
+    console.log(number);
+	var request= new XMLHttpRequest();
+	var databox = [];
+	request.onreadystatechange = function(){
+		if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
+			var data = JSON.parse(request.responseText);
+			var dataobject = {};
+			for(var i = 0; i< data.length; i++){
+                dataobject = {'id': data[i].id, 'image': data[i].image, 'likes': data[i].likes, 'missedIngredientCount': data[i].missedIngredientCount,'title': data[i].title,'usedIngredientCount': data[i].usedIngredientCount};
+                databox.push(dataobject);
+			}
+			var body = document.getElementById('body');
+            var table = document.createElement('table');
+            var thead = document.createElement('thead');
+            var trhead = document.createElement('tr');
+            var thnames = ['id','imgage','Missing Ingredient Count', 'Dish Name','Used Ingredient Count'];
+            for(var k = 0; k <thnames.length;k++){
+            	var th = document.createElement('th');
+            	th.innerHTML = thnames[k];
+            	trhead.appendChild(th);
+			}
+			thead.appendChild(trhead);
+			table.appendChild(thead);
+            var tbody = document.createElement('tbody');
+			for(var j = 0; j< databox.length;j++){
+				var tr = document.createElement('tr');
+				var tdid = document.createElement('td');
+				var tdimg =document.createElement('td');
+				var tdmic = document.createElement('td');
+				var tdt = document.createElement('td');
+				var tduic = document.createElement('td');
+				tdid.innerHTML = "<a href="+"recipeinfo.html?"+databox[j].id+">"+databox[j].id+"";
+				tdimg.innerHTML = ("<img src="+databox[j].image+">");
+				tdmic.innerHTML = (databox[j].missedIngredientCount);
+				tdt.innerHTML = (databox[j].title);
+				tduic.innerHTML = (databox[j].usedIngredientCount);
+				tr.appendChild(tdid);
+				tr.appendChild(tdid);
+				tr.appendChild(tdimg);
+				tr.appendChild(tdmic);
+				tr.appendChild(tdt);
+				tr.appendChild(tduic);
+				tbody.append(tr);
+			}
+			table.appendChild(tbody);
+			body.appendChild(table);
+		}
+	};
+	request.open("GET","https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients="+fillIngredients+"&ingredients="+Ingredients+"&limitLicense="+limitLicense+"&number="+number+"&ranking="+ranking+"");
+    request.setRequestHeader('X-Mashape-Key', 'bO9COj191VmshVzI4cNTPoxIxXoMp1ZfMQtjsnsM1bEzjBrumx');
+    request.setRequestHeader('Accept', 'application/json');
+	request.send();
 }
 
+function moreinfo() {
+    console.log(document.URL);
+    var id = document.URL.split('?');
+    console.log(id[1]);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            var recipeinfo = JSON.parse(request.responseText);
+            console.log(recipeinfo);
+            document.write("Title: " + recipeinfo.title + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("credit to: " + recipeinfo.creditText + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Dairy Free: " + recipeinfo.dairyFree + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Gluten Free: " + recipeinfo.glutenFree + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Id: " + recipeinfo.id + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Ready In: " + recipeinfo.readyInMinutes + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Servings: " + recipeinfo.servings + "<br>");
+            document.write("....................................................................."+ "<br>");
+            document.write("Ingredients: <br>");
+            for (var i = 0; i < recipeinfo.extendedIngredients.length; i++) {
+                document.write("name: " +recipeinfo.extendedIngredients[i].name+ "<br>");
+                document.write("amount: " +recipeinfo.extendedIngredients[i].amount+ "<br>");
+                document.write("preparation: " + recipeinfo.extendedIngredients[i].originalString + "<br>");
+                document.write("....................................................................."+ "<br>");
+            }
+            document.write("Instructions: " + recipeinfo.instructions);
+        }
+    };
+        request.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + id[1] + "/information");
+        request.setRequestHeader('X-Mashape-Key', 'bO9COj191VmshVzI4cNTPoxIxXoMp1ZfMQtjsnsM1bEzjBrumx');
+        request.setRequestHeader('Accept', 'application/json');
+        request.send();
+}
