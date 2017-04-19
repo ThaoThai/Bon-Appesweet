916fd95836daa9d94f7d31ff17de531b2e41f88a
Thao Thai <thai.thao9@gmail.com>
2017-04-19 15:43:41 -0500

    thao updates

--- Bon-Appesweet-Thao/javascript/search.js
+++ Bon-Appesweet-Thao/javascript/search.js
@@ -18,28 +18,23 @@ function moreinfo() {
             title.innerHTML = recipeinfo.title;
             var ingredients = "";
             for (var i = 0; i < recipeinfo.extendedIngredients.length; i++) {
-                ingredients += (recipeinfo.extendedIngredients[i].originalString + " " + recipeinfo.extendedIngredients[i].name+ "<br>");
+                ingredients += (recipeinfo.extendedIngredients[i].originalString + "<br>");
             }
             
-            var instruction = recipeinfo.instructions.replace("min.", "minutes").split(".");
-            var split_instruction="";
-            for (var j = 0; j < instruction.length-1; j++) {
-                var k = j + 1;
-                split_instruction +=
-                    "<h6>" + String(k) + "</h6>" +
-                    instruction[j] + "<br>";
-            }
+            var instruction = recipeinfo.instructions.replace("min.", "minutes");
             getAverageRating(id[1],function(err,rating){
-                description.innerHTML=("<p>RATING ="+rating+"</p>"+
-                "<h2> " + recipeinfo.title + "</h2><br>" +
-                "<img src="+ "'" + recipeinfo.image + "'" + "width='40%' height='40%'> <br> " +
-                "<h5> ..................................................................... </h5><br>" + "<h4> TOTAL TIME: </h4>" +
-                recipeinfo.readyInMinutes + " minutes" + "\t" + " <h4> YIELD: </h4>" + recipeinfo.servings + " servings <br>"+
-                "<h5>..................................................................... </h5><br>" +
+            
+                description.innerHTML=(
+                    "<p>RATING ="+rating+"</p>"+
+                    "<h2> " + recipeinfo.title + "</h2><br>" +
+                    "<img src="+ "'" + recipeinfo.image + "'" + "width='40%' height='40%'> <br> " + 
+                    "<div style=\"padding-left:44%;padding-right:44%;width:800px; \"> <fieldset class=\"rating\"> <input type=\"radio\" id=\"star5\" name=\"rating\" value=\"5\"/> <label class = \"full\" for=\"star5\"></label> <input type=\"radio\" id=\"star4\" name=\"rating\" value=\"4\" /> <label class = \"full\" for=\"star4\"></label> <input type=\"radio\" id=\"star3\" name=\"rating\" value=\"3\" /><label class = \"full\" for=\"star3\"></label><input type=\"radio\" id=\"star2\" name=\"rating\" value=\"2\" /> <label class = \"full\" for=\"star2\"></label> <input type=\"radio\" id=\"star1\" name=\"rating\" value=\"1\" /><label class = \"full\" for=\"star1\"></label> </fieldset></div>" + "<br><br><br>" +
+                    " <h5> ............................................................. </h5><br>" + "<h4> | TOTAL TIME | </h4>" +
+                    translateHour(recipeinfo.readyInMinutes) + " minutes" + "\t" + " <h4> | YIELD | </h4>" + recipeinfo.servings + " servings <br>"+
+                    "<h5>............................................................</h5><br>" +
 
-                "<h4> INGREDIENTS: </h4><br>" + ingredients + "<br>" + "<h4>INTRUCTIONS:  </h4><br>" + split_instruction);
+                 "<h4> | INGREDIENTS | </h4><br>" + ingredients + "<br>" + "<h4>| INTRUCTIONS |  </h4><br>" + instruction);
             });
-
             document.head.appendChild(title);
             body.append(description);
         }
@@ -54,13 +49,15 @@ function moreinfo() {
 function complexSearch(){
     var addRecipeInformation, cuisine, excludeIngredients, fillIngredients, includeIngredients,instructionRequired,intolerances,limitLicense,number,offset,query,ranking,type,readyInMinutes,timeCook, mealtype, minMinutes;
     addRecipeInformation = true;
+    var missed_ingredients = "";
+    var common_ingredients = ", onion, salt, pepper, garlic";
     readyInMinutes = 1;
     cuisine = document.getElementById('cuisine').value;
     //excludeIngredients = document.getElementById('excludeIngredients').value;
     fillIngredients = true;
     includeIngredients = document.getElementById('search').value;
     var IngredientCount;
-    number = 50;
+    number = 20000;
     IngredientCount = includeIngredients.split(',').length;
     console.log(IngredientCount);
     var replaceINgredients = replace(includeIngredients);
@@ -103,27 +100,42 @@ function complexSearch(){
             if(data.totalResults > 0) {
                 data.results.forEach(function (recipes) {
                     if (recipes.usedIngredientCount <= IngredientCount && recipes.usedIngredientCount > 0 && recipes.missedIngredientCount <= 2 && recipes.readyInMinutes > minMinutes && recipes.readyInMinutes <= readyInMinutes ){
+                        if (recipes.missedIngredientCount == 1) {
+                            missed_ingredients = recipes.missedIngredients[0].name;
+                        }
+                        else if (recipes.missedIngredientCount == 2) {
+                            missed_ingredients = recipes.missedIngredients[0].name + ", " + recipes.missedIngredients[1].name;
+                        }
+                        else if (recipes.missedIngredientCount==0){
+                            missed_ingredients="You have everything!";
+                        }
                         var recipediv = document.createElement('div');
                         console.log(recipes.title);
                         console.log(recipes.missedIngredientCount);
                         recipediv.setAttribute("id","recipediv");
+                        recipediv.innerHTML = (
+                            "<h4>" + recipes.title + "</h4>" +
+                            "<a href='recipeinfo.html?" + recipes.id + "' target='_blank'>" +
+                            "<img src="+ "'" + recipes.image + "'"+ "width='50%' height='50%'>" +"</a>" +
+                                "<h6> Missed ingredients: " + missed_ingredients +"</h6>");
                         getAverageRating(recipes.id,function(err,rating){
                             recipediv.innerHTML = (
                             "<h4>" + recipes.title + "</h4>" +
-                            "<a href='recipeinfo.php?" + recipes.id + "' target='_blank'>" +
-                            "<img src="+ "'" + recipes.image + "'"+ "width='50%' height='50%'>" +"</a>" + "rating= "+rating[0]+"<br>"+
+                            "<a href='recipeinfo.html?" + recipes.id + "' target='_blank'>" +
+                            "<img src="+ "'" + recipes.image + "'"+ "width='50%' height='50%'>" +"</a>" +
+                                "<h6> Missed ingredients: " + missed_ingredients +"</h6>" + "<br>" + "rating= "+rating[0]+"<br>"+
                             "<button onclick='getRating("+recipes.id+","+1+");alert(\"Thanks for rating!\")'>1</button>"+
                             "<button onclick='getRating("+recipes.id+","+2+");alert(\"Thanks for rating!\")'>2</button>"+
                             "<button onclick='getRating("+recipes.id+","+3+");alert(\"Thanks for rating!\")'>3</button>"+
                             "<button onclick='getRating("+recipes.id+","+4+");alert(\"Thanks for rating!\")'>4</button>"+
                             "<button onclick='getRating("+recipes.id+","+5+");alert(\"Thanks for rating!\")'>5</button>");
                         });
-
-            } else{
-                return;
-            }
-            wrapper.append(recipediv);
-        });
+                        
+                    } else{
+                        return;
+                    }
+                    wrapper.append(recipediv);
+                });
             }else if(data.totalResults ===0){
                 var p = document.createElement('p');
                 p.innerHTML = "NO RECIPE FOUND";
@@ -158,6 +170,112 @@ function replace(ingredient){
     return result;
 }
 
+
+//function simpleSearch(){
+//    var addRecipeInformation, cuisine, excludeIngredients, fillIngredients, includeIngredients,instructionRequired,intolerances,limitLicense,number,offset,query,ranking,type,readyInMinutes,timeCook, mealtype, minMinutes;
+//    addRecipeInformation = true;
+//    readyInMinutes = 1;
+//    cuisine = document.getElementById('cuisine').value;
+//    //excludeIngredients = document.getElementById('excludeIngredients').value;
+//    fillIngredients = true;
+//    includeIngredients = document.getElementById('search').value;
+//    var IngredientCount;
+//    number = 300000;
+//    IngredientCount = includeIngredients.split(',').length;
+//    console.log(IngredientCount);
+//    var replaceINgredients = replace(includeIngredients);
+//    console.log(replaceINgredients);
+//    instructionRequired = true;
+//    //intolerances = document.getElementById('intolerance').value;
+//    limitLicense =false;
+//    timeCook = document.getElementById('time').value;
+//    if (timeCook  == "Fast") {
+//        minMinutes = 0;
+//        readyInMinutes = 30;
+//    }
+//    else if (timeCook  == "Medium") {
+//        minMinutes = 0;
+//        readyInMinutes = 60;
+//    }
+//    else if (timeCook  == "Slow") {
+//        minMinutes = 90;
+//        readyInMinutes=300;
+//    }
+//    else {
+//        minMinutes =0;
+//        readyInMinutes=400;
+//    }
+//    
+//    offset=0;
+//    //query=document.getElementById('query').value;
+//    ranking=1;
+//    mealtype=document.getElementById('meal').value;
+//    //readyInMinutes=document.getElementById('readyInMinutes').value;
+//    var request = new XMLHttpRequest();
+//    request.onreadystatechange = function(){
+//        if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
+//            var data = JSON.parse(request.responseText);
+//            console.log(data);
+//            console.log(minMinutes);
+//            console.log(readyInMinutes);
+//            var wrapper = document.getElementById('recipes');
+//            wrapper.innerHTML="";
+//            if(data.totalResults > 0) {
+//                data.results.forEach(function (recipes) {
+//                    if (recipes.usedIngredientCount <= IngredientCount && recipes.usedIngredientCount > 0 && recipes.readyInMinutes > minMinutes && recipes.readyInMinutes <= readyInMinutes ){
+//                        var recipediv = document.createElement('div');
+//                        console.log(recipes.title);
+//                        console.log(recipes.missedIngredientCount);
+//                        recipediv.setAttribute("id","recipediv");
+//                        recipediv.innerHTML = (
+//                            "<h4>" + recipes.title + "</h4>" +
+//                            "<a href='recipeinfo.html?" + recipes.id + "' target='_blank'>" +
+//                            "<img src="+ "'" + recipes.image + "'"+ "width='50%' height='50%'>" +"</a>");
+//                    } else{
+//                        return;
+//                    }
+//                    wrapper.append(recipediv);
+//                });
+//            }else if(data.totalResults ===0){
+//                var p = document.createElement('p');
+//                p.innerHTML = "NO RECIPE FOUND";
+//                body.appendChild(p);
+//            }
+//        }
+//    };
+//    request.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?" +
+//        "addRecipeInformation=" + addRecipeInformation + "&" +
+//        "cuisine="+ replace(cuisine)+"&" +
+//        //"excludeIngredients="+ replace(excludeIngredients)+"&" +
+//        "fillIngredients="+ fillIngredients+"&" +
+//        "includeIngredients="+ replace(includeIngredients)+"&" +
+//        "instructionsRequired="+ instructionRequired+"&" +
+//        //"intolerances="+ replace(intolerances)+"&" +
+//        "limitLicense="+ limitLicense+"&" +
+//        "number="+ number+"&" +
+//        "readyInMinutes="+readyInMinutes+"&"+
+//        "offset="+ offset+"&" +
+//        //"query="+ replace(query)+"&" +
+//        "ranking=1" + "&"+
+//        "type="+ mealtype);
+//    request.setRequestHeader('X-Mashape-Key', 'bO9COj191VmshVzI4cNTPoxIxXoMp1ZfMQtjsnsM1bEzjBrumx');
+//    request.setRequestHeader('Accept', 'application/json');
+//    request.send();
+//}
+
+
+function translateHour(time) {
+    if (time < 60) {
+        return time;
+    }
+    else {
+        var min = time%60;
+        var hour = Math.round(time/60);
+        var result = hour + " hour " + min;
+        return result;
+    }
+}
+
 function getAverageRating(id,callback) {
         var request = new XMLHttpRequest();
 
@@ -183,55 +301,4 @@ function getRating(id,rating){
 }
 
 
-
-//function search(fillIngredients,limitLicense,ranking){
-//    var Ingredients = document.getElementById("search").value;
-//    //Ingredients = Ingredients.replace(',','%');
-//    var number = document.getElementById("number").value;
-//    console.log(number);
-//	var request= new XMLHttpRequest();
-//	var databox = [];
-//	request.onreadystatechange = function(){
-//		if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
-//			var data = JSON.parse(request.responseText);
-//			var dataobject = {};
-//			if(data.length > 0){
-//                for(var i = 0; i< data.length; i++){
-//                    dataobject = {'id': data[i].id, 'image': data[i].image, 'likes': data[i].likes, 'missedIngredientCount': data[i].missedIngredientCount,'title': data[i].title,'usedIngredientCount': data[i].usedIngredientCount,'missedIngredients': data[i].missedIngredients};
-//                    databox.push(dataobject);
-//                }
-//                console.log(databox);
-//                var body = document.getElementById('recipes');
-//                for(var j = 0; j< databox.length;j++){
-//                    var button = document.createElement('BUTTON');
-//                    var recipediv = document.createElement('div');
-//                    recipediv.setAttribute("id","recipediv");
-//                    button.setAttribute("id", "btnrecipes");
-////                    tdid.innerHTML = "<a href="+"recipeinfo.html?"+databox[j].id+">"+databox[j].id+"";
-//                    recipediv.innerHTML = (
-//                    "<h4>" + databox[j].title + "</h4>" +
-//                        "<img src="+databox[j].image+ " width='200px' height='200px'>");
-//                    //tdmic.innerHTML = (databox[j].missedIngredientCount);
-//                    //button.innerHTML = (databox[j].title);
-////                    for(var k = 0; k<databox[j].missedIngredients.length;k++){
-////                        var tring = document.createElement('tr');
-////                        tring.innerHTML = (databox[j].missedIngredients[k].name);
-////                        tdmi.appendChild(tring);
-////                    }
-//                    body.appendChild(recipediv);
-//                }
-//                
-//            }else{
-//			    var body = document.getElementById('body');
-//			    var p = document.createElement('p');
-//			    p.innerHTML = "NO RECIPE AVAILABLE FOR INGREDIENTS: " + document.getElementById("search").value;
-//			    body.appendChild(p);
-//            }
-//		}
-//	};
-//	request.open("GET","https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients="+fillIngredients+"&ingredients="+Ingredients+"&limitLicense="+limitLicense+"&number="+number+"&ranking="+ranking+"");
-//    request.setRequestHeader('X-Mashape-Key', 'bO9COj191VmshVzI4cNTPoxIxXoMp1ZfMQtjsnsM1bEzjBrumx');
-//    request.setRequestHeader('Accept', 'application/json');
-//	request.send();
-//}
 
---
+++ Bon-Appesweet-Thao/php/rating.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Geovanni
+ * Date: 4/9/2017
+ * Time: 9:31 PM
+ */
+$x = $_REQUEST["function"];
+$y = $_REQUEST["id"];
+if($x == "getAverageRating"){
+    echo getAverageRating($y);
+}else if($x == "Rating"){
+    $z=$_REQUEST['rating'];
+    Rating($y,$z);
+}
+function Rating($id,$rating){
+    $db = 'ratingtable';
+    $conn = mysqli_connect('localhost', 'root', '', 'ratingtable');
+    if (!$conn) {
+        die("ERROR ON THE CONNECTION" . mysqli_error());
+    } else {
+        echo "Connected successfully\n";
+        $result = mysqli_query($conn, "SELECT * FROM `ratings` WHERE id= $id");
+        $row = mysqli_fetch_array($result);
+        if(!$row){
+            echo "NO DATA FOUND\n";
+            $total = $row['total']+$rating;
+            $count = $row['count']+1;
+            $average = $total/$count;
+            $insert = mysqli_query($conn, "INSERT INTO `ratings`(`id`, `total`, `average`, `count`) VALUES ($id,$rating,$average,$count)");
+        }else{
+            echo "VALUE FOUND\n";
+            $total = $row['total']+$rating;
+            $count = $row['count']+1;
+            $average = $total/$count;
+            $update = mysqli_query($conn,"UPDATE `ratings` SET `total`= $total,`average`=$average,`count`= $count WHERE `id`=$id");
+        }
+    }
+}
+function getAverageRating($id){
+    $db = 'ratingtable';
+    $conn = mysqli_connect('localhost', 'root', '', 'ratingtable');
+    if (!$conn) {
+        die("ERROR ON THE CONNECTION" . mysqli_error());
+    } else {
+        $result = mysqli_query($conn, "SELECT * FROM `ratings` WHERE id= $id");
+        $row = mysqli_fetch_array($result);
+        if(!$row){
+            return (int)0;
+        }else{
+            return (int)$row['average'];
+        }
+    }
+}
+?>
