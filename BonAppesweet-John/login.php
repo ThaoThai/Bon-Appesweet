464e89f0c2705d8765b83a2b31c23f657cbfc19d
johnordono <john.ordono@gmail.com>
2017-04-26 14:59:27 -0500

    Add files via upload

--- BonAppesweet-John/login.php
+++ BonAppesweet-John/login.php
@@ -1,23 +1,43 @@
-<!DOCTYPE HTML>
-
 <?php
+
+    $db = mysqli_connect("localhost", "root", "", "authentication");
     session_start();
 
     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']== true){
-        header("Location: mainpage.php");
-        
+        header("Location: main.php");    
     }
     
-    if(isset($_POST['username']) && isset($_POST['password']){
-        if($_POST['username'] == $username && $_POST['password'] == $password){
-            $_SESSION['logged_in'] = true;
-            header("Location: mainpage.php");
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        $myusername = mysqli_real_escape_string($db,$_POST['username']);
+        $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+        $passwordhash = md5($mypassword); 
+        
+        
+        
+        
+        
+        $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$passwordhash'";
+        
+        $result = mysqli_query($db, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $active = $row['active'];
+        
+        $count = mysqli_num_rows($result);
+        
+        if ($count == 1){
+            session_register("myusename");
+            $_SESSION['login_user'] = $myusername;
+            
+            header("location: welcome.php");
+        }
+        
+        else{
+            $error = "Your Usernam or Password is invalid";   
         }
     }
-   
-        
-    }
 ?>
+
+<!DOCTYPE HTML>
 
 <html>
 
@@ -30,11 +50,11 @@
 
 <body>    
     <div id = "frame">
-        <p style="text-align: center; font-family:helvetica; color:white">
+        <p style="text-align: center; font-family:helvetica; color:grey">
         User Login
         </p>
         <form method="post" action="index.php">
-            <p style= "text-align: left; font-family:helvetica; color:white">
+            <p style= "text-align: left; font-family:helvetica; color:grey">
             Username:<br/>
             <input type="text" name="username"><br/>
             Password:<br/>
